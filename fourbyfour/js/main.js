6f94e5963704d7127f0e5abfa83f1333bdc89119
jordan Moldovan <jmoldyvan@gmail.com>
2022-04-23 20:37:22 -0700

    update 0.04

--- fourbyfour/js/main.js
+++ fourbyfour/js/main.js
@@ -1,13 +1,18 @@
 let slot1 = document.querySelector('#slot1')
 let slot2 = document.querySelector('#slot2')
-let slot15 = document.querySelector('#slot15')
+let slot3 = document.querySelector('#slot3')
 let slot4 = document.querySelector('#slot4')
 let slot5 = document.querySelector('#slot5')
-let slot12 = document.querySelector('#slot12')
-let slot11 = document.querySelector('#slot11')
+let slot6 = document.querySelector('#slot6')
+let slot7 = document.querySelector('#slot7')
 let slot8 = document.querySelector('#slot8')
 let slot9 = document.querySelector('#slot9')
-
+let slot10 = document.querySelector('#slot10')
+let slot11 = document.querySelector('#slot11')
+let slot12 = document.querySelector('#slot12')
+let slot13 = document.querySelector('#slot13')
+let slot14 = document.querySelector('#slot14')
+let slot15 = document.querySelector('#slot15')
 
 
 
@@ -36,5 +41,28 @@ function drop(ev) {
         && slot15.getElementsByTagName('img').length > 0) {
         alert('you solved the riddle!')
     }
+    if (slot1.getElementsByTagName('img').length > 0
+        && slot3.getElementsByTagName('img').length > 0
+        && slot4.getElementsByTagName('img').length > 0
+        && slot5.getElementsByTagName('img').length > 0
+        && slot8.getElementsByTagName('img').length > 0
+        && slot9.getElementsByTagName('img').length > 0
+        && slot11.getElementsByTagName('img').length > 0
+        && slot12.getElementsByTagName('img').length > 0
+        && slot14.getElementsByTagName('img').length > 0) {
+        alert('you solved the riddle!')
+    }
+    if (slot1.getElementsByTagName('img').length > 0
+        && slot3.getElementsByTagName('img').length > 0
+        && slot4.getElementsByTagName('img').length > 0
+        && slot7.getElementsByTagName('img').length > 0
+        && slot8.getElementsByTagName('img').length > 0
+        && slot9.getElementsByTagName('img').length > 0
+        && slot10.getElementsByTagName('img').length > 0
+        && slot12.getElementsByTagName('img').length > 0
+        && slot13.getElementsByTagName('img').length > 0) {
+        alert('you solved the riddle!')
+    }
+
 }
 
