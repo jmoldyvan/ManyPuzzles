fd373a4305cd37b1e9b9e976dddf8f131faf3e77
jordan Moldovan <jmoldyvan@gmail.com>
2022-04-23 20:05:09 -0700

    update 0.04

---
+++ fourbyfour/js/main.js
@@ -0,0 +1,40 @@
+let slot1 = document.querySelector('#slot1')
+let slot2 = document.querySelector('#slot2')
+let slot15 = document.querySelector('#slot15')
+let slot4 = document.querySelector('#slot4')
+let slot5 = document.querySelector('#slot5')
+let slot12 = document.querySelector('#slot12')
+let slot11 = document.querySelector('#slot11')
+let slot8 = document.querySelector('#slot8')
+let slot9 = document.querySelector('#slot9')
+
+
+
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData('content', ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    if (ev.target.tagName == "IMG") { return; }
+    let data = ev.dataTransfer.getData("content")
+    ev.target.appendChild(document.getElementById(data)
+    );
+    if (slot1.getElementsByTagName('img').length > 0
+        && slot2.getElementsByTagName('img').length > 0
+        && slot4.getElementsByTagName('img').length > 0
+        && slot5.getElementsByTagName('img').length > 0
+        && slot8.getElementsByTagName('img').length > 0
+        && slot9.getElementsByTagName('img').length > 0
+        && slot11.getElementsByTagName('img').length > 0
+        && slot12.getElementsByTagName('img').length > 0
+        && slot15.getElementsByTagName('img').length > 0) {
+        alert('you solved the riddle!')
+    }
+}
+
